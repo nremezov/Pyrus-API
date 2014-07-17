49e73fb60d4cfc5c2bec642c88d675b8d55fe2b1
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2014-07-17 16:50:37 -0400

    added files

--- get_profile.rb
+++ get_profile.rb
@@ -6,15 +6,9 @@ conn = Faraday.new(url: 'https://pyrus.com') do |conn|
   conn.adapter  Faraday.default_adapter
 end
 
-create_comment = conn.put do |req|
-	req.url '/restapi/addcomment'
-	req.headers['Content-Type'] = 'application/json'
+get_profile = conn.get do |req|
+	req.url '/restapi/profile'
 	req.headers['Authorization'] = 'Bearer {Access Token}' # Example => req.headers['Authorization'] = 'Bearer fb2e77d.47a0479'
-	req.body = '{
-    "Text": "Hello World",
-    "TaskId": 4038676, # => Task ID is needed 
-    "ActivityAction": "MarkAsIrrelevant"
-}'
 end 
 
-puts create_comment.body
+puts get_profile.body
