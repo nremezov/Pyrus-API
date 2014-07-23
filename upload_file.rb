91abb7219054314415d082a898fc8b029e97f0c7
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2014-07-23 11:38:16 -0400

    blank file

--- upload_file.rb
+++
@@ -1,8 +0,0 @@
-require 'faraday'
-
-conn = Faraday.new(url: 'https://pyrus.com') do |conn|
-  conn.request  :url_encoded           
-  conn.response :logger             
-  conn.adapter  :net_http
-end
-
